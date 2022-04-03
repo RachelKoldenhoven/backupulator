5d452452fee55bef09b3bee882e9317d2e7a7f7f
Brent Gardner <bgardner@squarelabs.net>
2022-04-03 13:50:44 -0600

    Share context

--- app.mjs
+++ app.mjs
@@ -2,6 +2,38 @@ import Vue from './node_modules/vue/dist/vue.esm.browser.js';
 
 const root = '/Volumes/BACKUPA/Backup/Shared/Pictures';
 
+const scan = async (directoryName, hashes, cb) => {
+    let dir = await electron.fsp.readdir(directoryName, {withFileTypes: true});
+    for (const f of dir) {
+        try {
+            const fullPath = electron.path.join(directoryName, f.name);
+            const ext = electron.path.extname(f.name).toLocaleLowerCase();
+            if (f.isDirectory()) {
+                await scan(fullPath, hashes, cb);
+            } else if (['.jpg', '.jpeg'].includes(ext)) {
+                // await changeExif(fullPath);
+
+                const file = {
+                    name: f.name,
+                }
+                try {
+                    const exif = electron.getExifFromJpegFile(fullPath);
+                    const imageUniqueId = exif.Exif[electron.piexif.ExifIFD.ImageUniqueID]
+                    file.imageUniqueId = imageUniqueId;
+                    hashes[imageUniqueId] = hashes[imageUniqueId] || [];
+                    hashes[imageUniqueId].push(file);
+                    console.log(imageUniqueId)
+                } catch (e) {
+                    console.error(`Error unpacking ${f.name}`, e)
+                }
+            }
+        } catch(ex) {
+            console.error(`Error on file ${f}`, ex);
+        }
+    }
+    cb(hashes);
+}
+
 onload = () => {
     const app = new Vue({
         el: '#app',
@@ -18,7 +50,7 @@ onload = () => {
         },
         async created() {
             this.hashes = {}
-            electron.scan(root, this.hashes, (hashes) => this.hashes = hashes);
+            scan(root, this.hashes, (hashes) => this.hashes = hashes);
         },
     });
 };
--- preload.js
+++ preload.js
@@ -1,46 +1,46 @@
-const { contextBridge } = require('electron');
 const fs = require('fs');
 const path = require("path");
 const piexif = require('piexifjs');
-
+// const { imageHash } = require('image-hash');
 const fsp = fs.promises;
+
+const hasher = new Worker('hasher.js');
+hasher.postMessage([0, 1]);
+console.log('Message posted to worker');
+
+const imgHash = (path) => {
+  return new Promise((res, rej) => {
+    imageHash(path, 16, true, (error, data) => {
+      if (error) rej(error);
+      res(data);
+    });
+  })
+}
+
+const changeExif = async (fullPath) => {
+  const exif = getExifFromJpegFile(fullPath);
+  if(exif.Exif[piexif.ExifIFD.ImageUniqueID]) {
+    console.log(`Skipping ${fullPath}`);
+    return;
+  }
+
+  console.log(`Hashing ${fullPath}`);
+  const hash = await imgHash(fullPath);
+  exif.Exif[piexif.ExifIFD.ImageUniqueID] = hash;
+
+  const newImageData = getBase64DataFromJpegFile(fullPath);
+  const newExifBinary = piexif.dump(exif);
+  const newPhotoData = piexif.insert(newExifBinary, newImageData);
+  const fileBuffer = Buffer.from(newPhotoData, 'binary');
+  fs.writeFileSync(fullPath, fileBuffer);
+};
 
 const getBase64DataFromJpegFile = filename => fs.readFileSync(filename).toString('binary');
 const getExifFromJpegFile = filename => piexif.load(getBase64DataFromJpegFile(filename));
-const scan = async (directoryName, hashes, cb) => {
-  let dir = await fsp.readdir(directoryName, {withFileTypes: true});
-  for (const f of dir) {
-    try {
-      const fullPath = path.join(directoryName, f.name);
-      const ext = path.extname(f.name).toLocaleLowerCase();
-      if (f.isDirectory()) {
-        await scan(fullPath, hashes, cb);
-      } else if (['.jpg', '.jpeg'].includes(ext)) {
-        const file = {
-          name: f.name,
-        }
-        try {
-          const exif = getExifFromJpegFile(fullPath);
-          const imageUniqueId = exif.Exif[piexif.ExifIFD.ImageUniqueID]
-          file.imageUniqueId = imageUniqueId;
-          hashes[imageUniqueId] = hashes[imageUniqueId] || [];
-          hashes[imageUniqueId].push(file);
-          console.log(imageUniqueId)
-        } catch (e) {
-          console.error(`Error unpacking ${f.name}`, e)
-        }
-      }
-    } catch(ex) {
-      console.error(`Error on file ${f}`, ex);
-    }
-  }
-  cb(hashes);
-}
 
-contextBridge.exposeInMainWorld('electron', {
+window.electron = {
   fsp,
   path,
   piexif,
   getExifFromJpegFile,
-  scan,
-});
+}
